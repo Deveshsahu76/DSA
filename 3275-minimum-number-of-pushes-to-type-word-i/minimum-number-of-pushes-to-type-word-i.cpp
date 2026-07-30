b0e838af5b7c656278617ff8d9940769178bd25e
Devesh Sahu <154410796+Deveshsahu76@users.noreply.github.com>
2026-07-30 18:02:39 +0530

    Time: 4 ms (3.18%) | Memory: 8.2 MB (77.73%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            answer += (i / 8) + 1;
+        }
+
+        return answer;
+    }
+};
