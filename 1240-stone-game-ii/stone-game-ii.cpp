d6d7b4fccf87c2df4919c2f040c8d27776807058
Devesh Sahu <154410796+Deveshsahu76@users.noreply.github.com>
2026-08-09 20:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (43.80%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int n;
+    vector<int> suffix;
+    vector<vector<int>> dp;
+
+    int solve(int i, int M) {
+
+        // Agar remaining piles 2*M ya usse kam hain,
+        // Alice sab le sakti hai.
+        if (i + 2 * M >= n) {
+            return suffix[i];
+        }
+
+        if (dp[i][M] != -1) {
+            return dp[i][M];
+        }
+
+        int ans = 0;
+
+        for (int X = 1; X <= 2 * M; X++) {
+
+            int nextM = max(M, X);
+
+            int alice = suffix[i]
+                      - solve(i + X, nextM);
+
+            ans = max(ans, alice);
+        }
+
+        return dp[i][M] = ans;
+    }
+
+    int stoneGameII(vector<int>& piles) {
+
+        n = piles.size();
+
+        // suffix[i] = piles[i] + piles[i+1] + ...
+        suffix.resize(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        dp.assign(n, vector<int>(n + 1, -1));
+
+        return solve(0, 1);
+    }
+};
